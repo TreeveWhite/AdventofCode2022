82dbe20d87376d6a0eb48eac39dea94c6ee8bbe2
TWhite <47389365+TreeveWhite@users.noreply.github.com>
2022-12-06 14:16:36 +0000

    Complete day 6

--- Day6/main.py
+++ Day6/main.py
@@ -1,12 +1,28 @@
 
+def get_marker(marker_length, message):
+    num_char = 0
+
+    while num_char <= len(message)-marker_length:
+        chars = {message[num_char]}
+
+        for i in range(1, marker_length):
+            chars.add(message[num_char+i])
+
+        if len(chars) == marker_length:
+            break
+        else:
+            num_char += 1
+
+    return num_char + marker_length
 
 def main():
 
     with open("data.txt", "r") as file:
-        data = file.readlines()
+        data = file.read()
+    
+    #return get_marker(4, data)
 
+    return get_marker(14, data)
 
 if __name__ == "__main__":
-    resp = main()
-
-    print(resp)
+    print(main())
